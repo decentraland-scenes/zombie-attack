2d4ca2338b5fa958dc7081d586d0b944f1ed5a5a
nearnshaw <nearnshaw@decentraland.org>
2022-02-15 16:22:27 -0300

    update SDK  & fix animations

--- src/zombie.ts
+++ src/zombie.ts
@@ -6,28 +6,28 @@ export class Zombie extends Entity {
     this.addComponent(transform)
 
     this.addComponent(new Animator())
-    this.getComponent(Animator).addClip(new AnimationState("Walking", { looping: true }))
-    this.getComponent(Animator).addClip(new AnimationState("Attacking", { looping: true }))
-    this.getComponent(Animator).getClip("Walking").play()
+    this.getComponent(Animator).addClip(
+      new AnimationState('Walking', { looping: true })
+    )
+    this.getComponent(Animator).addClip(
+      new AnimationState('Attacking', { looping: true })
+    )
+    this.getComponent(Animator).getClip('Walking').play()
   }
 
   // Play attacking animation
   attack() {
-    this.stopAnimations()
-    this.getComponent(Animator).getClip("Attacking").play()
+    this.getComponent(Animator).getClip('Attacking').play()
   }
 
   // Play walking animation
   walk() {
-    this.stopAnimations()
-    this.getComponent(Animator).getClip("Walking").play()
+    this.getComponent(Animator).getClip('Walking').play()
   }
 
   // Bug workaround: otherwise the next animation clip won't play
   stopAnimations() {
-    this.getComponent(Animator).getClip("Walking").stop()
-    this.getComponent(Animator).getClip("Attacking").stop()
+    this.getComponent(Animator).getClip('Walking').stop()
+    this.getComponent(Animator).getClip('Attacking').stop()
   }
 }
-
-
