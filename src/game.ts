6d45a54cd09a061c62bd58708ea9395a0db968fd
nearnshaw <nearnshaw@decentraland.org>
2022-03-24 17:02:17 -0300

    lint fix and remove package-lock.json (if aplies)

--- src/game.ts
+++ src/game.ts
@@ -1,20 +1,20 @@
-import { Zombie } from "./zombie"
+import { Zombie } from './zombie'
 
 // Base
 const base = new Entity()
-base.addComponent(new GLTFShape("models/baseLight.glb"))
+base.addComponent(new GLTFShape('models/baseLight.glb'))
 base.addComponent(
   new Transform({
-    scale: new Vector3(2, 1, 2),
+    scale: new Vector3(2, 1, 2)
   })
 )
 engine.addEntity(base)
 
 // Zombie
 const zombie = new Zombie(
-  new GLTFShape("models/zombie.glb"),
+  new GLTFShape('models/zombie.glb'),
   new Transform({
-    position: new Vector3(16, 0.933, 16),
+    position: new Vector3(16, 0.933, 16)
   })
 )
 
@@ -29,16 +29,28 @@ const transform = zombie.getComponent(Transform)
 class ZombieAttack implements ISystem {
   update(dt: number) {
     // Rotate to face the player
-    let lookAtTarget = new Vector3(player.position.x, transform.position.y, player.position.z)
-    let direction = lookAtTarget.subtract(transform.position)
-    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), dt * ROT_SPEED)
+    const lookAtTarget = new Vector3(
+      player.position.x,
+      transform.position.y,
+      player.position.z
+    )
+    const direction = lookAtTarget.subtract(transform.position)
+    transform.rotation = Quaternion.Slerp(
+      transform.rotation,
+      Quaternion.LookRotation(direction),
+      dt * ROT_SPEED
+    )
 
     // Continue to move towards the player until it is within 2m away
-    let distance = Vector3.DistanceSquared(transform.position, player.position) // Check distance squared as it's more optimized
-    if (distance >= 4) { // Note: Distance is squared so a value of 4 is when the zombie is standing 2m away
+    const distance = Vector3.DistanceSquared(
+      transform.position,
+      player.position
+    ) // Check distance squared as it's more optimized
+    if (distance >= 4) {
+      // Note: Distance is squared so a value of 4 is when the zombie is standing 2m away
       zombie.walk()
-      let forwardVector = Vector3.Forward().rotate(transform.rotation)
-      let increment = forwardVector.scale(dt * MOVE_SPEED)
+      const forwardVector = Vector3.Forward().rotate(transform.rotation)
+      const increment = forwardVector.scale(dt * MOVE_SPEED)
       transform.translate(increment)
     } else {
       zombie.attack()
